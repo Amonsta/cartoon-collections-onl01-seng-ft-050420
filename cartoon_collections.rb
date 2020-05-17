26ab15f15d491a4b1001d858253ee62f61f47c23
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-17 14:09:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(array)    # code an argument here
-  array.count 
+  array.string_length
+  
 end
 
 def long_planeteer_calls# code an argument here
