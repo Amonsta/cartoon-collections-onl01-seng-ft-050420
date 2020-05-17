0df0d012b0cb580229c00c79ef232d08adfe9c77
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-17 16:58:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,13 +26,11 @@ def long_planeteer_calls(calls_short)# code an argument here
   end  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  .find 
-  
-  else
-    nil
+    cheese.find do |type|
+    cheese_types.include?(type)
+  end
   
 end
